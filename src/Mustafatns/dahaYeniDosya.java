225df2ddc5f5c824b23642d544b7d959cc7ac48d
Suveyda <suveydagulec@gmail.com>
2020-11-22 20:00:53 -0500

    more changes2

--- src/Mustafatns/dahaYeniDosya.java
+++ src/Mustafatns/dahaYeniDosya.java
@@ -4,5 +4,6 @@ public class dahaYeniDosya {
     public static void main(String[] args) {
         System.out.println("daha cok deneme");
         System.out.println("deneyerek ogreniyoruz");
+        //this is a test
     }
 }
