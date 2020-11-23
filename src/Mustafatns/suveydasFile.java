765f2083b36a0ed097e7896fc88af9666e5a51e5
Suveyda <suveydagulec@gmail.com>
2020-11-22 19:59:14 -0500

    new commit

--- src/Mustafatns/suveydasFile.java
+++ src/Mustafatns/suveydasFile.java
@@ -3,5 +3,6 @@ package Mustafatns;
 public class suveydasFile {
     public static void main(String[] args) {
         System.out.println("hellooo");
+        System.out.println("more changes");
     }
 }
